b9947ac29d55935b4feb33b9fc70b0ecdf92f663
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2025-12-10 21:11:13 -0800

    chore: Update ggen to v3.4.1 with clap-noun-verb v5.3.2

--- Formula/ggen.rb
+++ Formula/ggen.rb
@@ -1,8 +1,8 @@
 class Ggen < Formula
   desc "Deterministic, language-agnostic code generation framework"
   homepage "https://github.com/seanchatmangpt/ggen"
-  url "https://github.com/seanchatmangpt/ggen/archive/refs/tags/v2.5.0.tar.gz"
-  sha256 "faaccded2180c337404eb66bded839690a006a9dafbc729fbc8af3844f5ea504"
+  url "https://github.com/seanchatmangpt/ggen/archive/refs/tags/v3.4.1.tar.gz"
+  sha256 "4b6e49c7c5becbc1812d3ff43a9ab4d664fb0bba4160545540a9242520795cae"
   license "MIT"
   head "https://github.com/seanchatmangpt/ggen.git", branch: "master"
 
